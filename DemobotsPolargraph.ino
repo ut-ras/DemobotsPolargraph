a0178b19cd140b4b8c167cefd0303208d6d69a12
cole-thompson <colethompson@utexas.edu>
2018-02-15 18:58:21 -0600

    setting up a test

--- DemobotsPolargraph.ino
+++ DemobotsPolargraph.ino
@@ -1,11 +1,41 @@
+#include <Adafruit_MotorShield.h>
+
 //DEMOBOTS POLARGRAPH :)
 
-void setup() {
-  // put your setup code here, to run once:
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_MS_PWMServoDriver.h"
 
+/* Install Motor Shield Library (Sketch->Tools->Manage Libraries)
+ * https://learn.adafruit.com/adafruit-motor-shield-v2-for-arduino/install-software
+ * Stepper Motor reference
+ * https://learn.adafruit.com/adafruit-motor-shield-v2-for-arduino/using-stepper-motors
+ */
+
+/* By default, the motor will 'hold' the position after its done stepping
+ * If you want to release all the coils, so that it can spin freely, call release()
+ */
+ 
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+Adafruit_StepperMotor *left;
+Adafruit_StepperMotor *right;
+    
+void setup() {
+    Serial.begin(9600);
+    Serial.println("setup starting");
+
+    left = AFMS.getStepper(200, 1);
+    right = AFMS.getStepper(200, 2);
+  
+    AFMS.begin();
+    
+    right->release();
+    Serial.print("setup finished");
+    
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  right->step(200, FORWARD, SINGLE);
+  delay(100);
 }
