5ad29e7c67bf308197c2d16b4ba08699be585419
cole-thompson <colethompson@utexas.edu>
2018-02-15 20:36:39 -0600

    set up test for AccelStepper library

--- DemobotsPolargraph.ino
+++ DemobotsPolargraph.ino
@@ -1,13 +1,15 @@
 //DEMOBOTS POLARGRAPH :)
 
 #include <Wire.h>
+#include <AccelStepper.h>
 #include <Adafruit_MotorShield.h>
-#include "utility/Adafruit_MS_PWMServoDriver.h"
 
 /* Install Motor Shield Library (Sketch->Tools->Manage Libraries)
  * https://learn.adafruit.com/adafruit-motor-shield-v2-for-arduino/install-software
- * Stepper Motor reference
- * https://learn.adafruit.com/adafruit-motor-shield-v2-for-arduino/using-stepper-motors
+ * Install AccelStepper Library (Sketch->Tools->Manage Libraries)
+ * 
+ * Testing a stepper: File>Examples>Adafruit Motor Shield V2 > StepperTest
+ * AF Stepper Motor reference https://learn.adafruit.com/adafruit-motor-shield-v2-for-arduino/using-stepper-motors
  */
 
 /* By default, the motor will 'hold' the position after its done stepping
@@ -15,40 +17,83 @@
  */
  
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
-Adafruit_StepperMotor *left = AFMS.getStepper(200, 1);
-Adafruit_StepperMotor *right = AFMS.getStepper(200, 2);
-    
+//Adafruit_StepperMotor *leftStepperAF = AFMS.getStepper(200, 1);
+Adafruit_StepperMotor *rightStepperAF = AFMS.getStepper(200, 2);
+
+//wrappers for AccelStepper objects
+/*
+void forwardstepL() {  
+  leftStepperAF->onestep(FORWARD, SINGLE);
+}
+void backwardstepL() {  
+  leftStepperAF->onestep(BACKWARD, SINGLE);
+}
+*/
+void forwardstepR() {  
+  rightStepperAF->onestep(FORWARD, DOUBLE);
+}
+void backwardstepR() {  
+  rightStepperAF->onestep(BACKWARD, DOUBLE);
+}
+
+//AccelStepper stepperL(forwardstepL, backwardstepL);
+AccelStepper stepperR(forwardstepR, backwardstepR);
+
 void setup() {
     Serial.begin(9600);
     Serial.println("setup starting");
   
     AFMS.begin();
-    
-    right->setSpeed(10);
-    //right->release();
+
+    /* stepperL.setMaxSpeed(100.0);
+    stepperL.setAcceleration(100.0);
+    stepperL.moveTo(24); */
+      
+    stepperR.setMaxSpeed(200.0);
+    stepperR.setAcceleration(100.0);
+    stepperR.moveTo(50000);
     
     Serial.println("setup finished");
     
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  testAccelStepperLib();
+  //testAFLib();
+}
 
+
+//test for AccelStepper library
+void testAccelStepperLib() {
+  //if (stepperL.distanceToGo() == 0)
+  //  stepperL.moveTo(-stepperL.currentPosition());
+  
+  if (stepperR.distanceToGo() == 0)
+    stepperR.moveTo(-stepperR.currentPosition());
+
+  //stepperL.run();
+  stepperR.run();
+}
+
+
+//test for default adafruit motor shield library
+void testAFLib() {
   Serial.println("Single coil steps");
-  right->step(100, FORWARD, SINGLE); 
-  right->step(100, BACKWARD, SINGLE); 
+  rightStepperAF->step(100, FORWARD, SINGLE); 
+  rightStepperAF->step(100, BACKWARD, SINGLE); 
 
   Serial.println("Double coil steps");
-  right->step(100, FORWARD, DOUBLE); 
-  right->step(100, BACKWARD, DOUBLE);
+  rightStepperAF->step(100, FORWARD, DOUBLE); 
+  rightStepperAF->step(100, BACKWARD, DOUBLE);
   
   Serial.println("Interleave coil steps");
-  right->step(100, FORWARD, INTERLEAVE); 
-  right->step(100, BACKWARD, INTERLEAVE); 
+  rightStepperAF->step(100, FORWARD, INTERLEAVE); 
+  rightStepperAF->step(100, BACKWARD, INTERLEAVE); 
   
   Serial.println("Microstep steps");
-  right->step(50, FORWARD, MICROSTEP); 
-  right->step(50, BACKWARD, MICROSTEP);
-  
-  delay(1000);
+  rightStepperAF->step(50, FORWARD, MICROSTEP); 
+  rightStepperAF->step(50, BACKWARD, MICROSTEP);
 }
+
+
+
