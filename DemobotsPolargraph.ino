c83ef82867706b2d485d00c7a1ef89890efa8c96
cole-thompson <colethompson@utexas.edu>
2018-03-01 21:51:15 -0600

    h-bridge motor test

--- DemobotsPolargraph.ino
+++ DemobotsPolargraph.ino
@@ -1,57 +1,34 @@
-//DEMOBOTS POLARGRAPH :)
-
-#include <Wire.h>
-#include <AccelStepper.h>
-#include <Adafruit_MotorShield.h>
-
-/* Install Motor Shield Library (Sketch->Tools->Manage Libraries)
- * https://learn.adafruit.com/adafruit-motor-shield-v2-for-arduino/install-software
- * Install AccelStepper Library (Sketch->Tools->Manage Libraries)
+/* DEMOBOTS POLARGRAPH :) 2018
  * 
- * Testing a stepper: File>Examples>Adafruit Motor Shield V2 > StepperTest
- * AF Stepper Motor reference https://learn.adafruit.com/adafruit-motor-shield-v2-for-arduino/using-stepper-motors
- */
-
-/* By default, the motor will 'hold' the position after its done stepping
- * If you want to release all the coils, so that it can spin freely, call release()
  */
  
-Adafruit_MotorShield AFMS = Adafruit_MotorShield();
-Adafruit_StepperMotor *leftStepperAF = AFMS.getStepper(200, 1);
-Adafruit_StepperMotor *rightStepperAF = AFMS.getStepper(200, 2);
+#include <Wire.h>
+#include <AccelStepper.h>
+//#include <Adafruit_MotorShield.h>
 
-//wrappers for AccelStepper objects
+/*
+ * Install AccelStepper Library (Sketch->Tools->Manage Libraries)
+ *    Cool library for interleaving steppers, easy to use with different motor drivers
+ */
 
-void forwardstepL() {  
-  leftStepperAF->onestep(FORWARD, SINGLE);
-}
-void backwardstepL() {  
-  leftStepperAF->onestep(BACKWARD, SINGLE);
-}
 
-void forwardstepR() {  
-  rightStepperAF->onestep(FORWARD, SINGLE);
-}
-void backwardstepR() {  
-  rightStepperAF->onestep(BACKWARD, SINGLE);
-}
-
-AccelStepper stepperL(forwardstepL, backwardstepL);
-AccelStepper stepperR(forwardstepR, backwardstepR);
+//H-Bridge AccelStepper objects
+AccelStepper stepperL(4, 0, 1, 2, 3);
+AccelStepper stepperR(4, 4, 5, 6, 7);
 
 void setup() {
     Serial.begin(9600);
     Serial.println("setup starting");
   
-    AFMS.begin();
+    //AFMS.begin();     //for Adafruit Motor Shield (see below)
 
     stepperL.setMaxSpeed(200.0);
     stepperL.setAcceleration(100.0);
-    stepperL.moveTo(5000);
+    stepperL.moveTo(1000);
       
     stepperR.setMaxSpeed(200.0);
     stepperR.setAcceleration(100.0);
-    stepperR.moveTo(5000);
+    stepperR.moveTo(1000);
     
     Serial.println("setup finished");
     
@@ -59,7 +36,8 @@ void setup() {
 
 void loop() {
   testAccelStepperLib();
-  //testAFLib();
+
+
 }
 
 
@@ -77,25 +55,35 @@ void testAccelStepperLib() {
 
 
 
-
-//test for default adafruit motor shield library
-void testAFLib() {
-  Serial.println("Single coil steps");
-  rightStepperAF->step(100, FORWARD, SINGLE); 
-  rightStepperAF->step(100, BACKWARD, SINGLE); 
-
-  Serial.println("Double coil steps");
-  rightStepperAF->step(100, FORWARD, DOUBLE); 
-  rightStepperAF->step(100, BACKWARD, DOUBLE);
-  
-  Serial.println("Interleave coil steps");
-  rightStepperAF->step(100, FORWARD, INTERLEAVE); 
-  rightStepperAF->step(100, BACKWARD, INTERLEAVE); 
-  
-  Serial.println("Microstep steps");
-  rightStepperAF->step(50, FORWARD, MICROSTEP); 
-  rightStepperAF->step(50, BACKWARD, MICROSTEP);
+/* //Adafruit Motor Shield v2 initialization - move to top for AFMS
+   //Not using AFMS until we get a new one, board is broken, short across m3m4
+   //We can easily replace the H-Bridge AccelStepper objects once we fix motor shield
+   
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+Adafruit_StepperMotor *leftStepperAF = AFMS.getStepper(200, 1);
+Adafruit_StepperMotor *rightStepperAF = AFMS.getStepper(200, 2);
+//wrappers for AccelStepper objects
+void forwardstepL() {  
+  leftStepperAF->onestep(FORWARD, SINGLE);
+}
+void backwardstepL() {  
+  leftStepperAF->onestep(BACKWARD, SINGLE);
+}
+void forwardstepR() {  
+  rightStepperAF->onestep(FORWARD, SINGLE);
+}
+void backwardstepR() {  
+  rightStepperAF->onestep(BACKWARD, SINGLE);
 }
 
+AccelStepper stepperL(forwardstepL, backwardstepL);
+AccelStepper stepperR(forwardstepR, backwardstepR);
+*/
 
+/* Install Motor Shield Library (Sketch->Tools->Manage Libraries)
+ * https://learn.adafruit.com/adafruit-motor-shield-v2-for-arduino/install-software
+ * 
+ * Testing a stepper: File>Examples>Adafruit Motor Shield V2 > StepperTest
+ * AF Stepper Motor reference https://learn.adafruit.com/adafruit-motor-shield-v2-for-arduino/using-stepper-motors
+ */
 
