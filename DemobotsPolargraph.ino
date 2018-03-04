5d4a956f822558d400972a675dc769c8a323fca1
Cole Thompson <colethompson@utexas.edu>
2018-03-03 19:20:15 -0600

    Cleanup

--- DemobotsPolargraph.ino
+++ DemobotsPolargraph.ino
@@ -77,9 +77,10 @@ pos pos_current = {0, 0};
 line line_current;
 polygon poly_current;
 
-//TEST
-polygon p;
-  
+//keep track of current string length, less computation
+int left_length;
+int right_length;
+    
 //H-Bridge AccelStepper objects 
 AccelStepper stepperL(4, 0, 1, 2, 3);
 AccelStepper stepperR(4, 4, 5, 6, 7);
@@ -112,6 +113,10 @@ void setupPolargraph() {
   line l = {p1, p2};
   setCurrentLine(l);*/
 
+  left_length = getLeftStringLength(pos_current);
+  right_length = getRightStringLength(pos_current);
+  
+  polygon p;
   pos points[5] = {{0, 0}, {0, 300}, {300, 300}, {300, 0}, {0, 0}};
   p.points_arr_size = 5;
   memcpy(p.points, points, sizeof(points[0]) * p.points_arr_size);
@@ -243,14 +248,14 @@ int getRightStringLength(pos pos_new) {
 //Set position of polargraph drawing instrument, return false if invalid pos
 bool setPos(pos pos_new) {
   if (isValidPos(pos_new)) {
-    int left_length = getLeftStringLength(pos_current);
-    int right_length = getRightStringLength(pos_current);
-
     int left_length_new= getLeftStringLength(pos_new);
     int right_length_new = getRightStringLength(pos_new);
 
     int left_steps = (left_length_new - left_length) * mm_to_steps_pulley;
     int right_steps = (right_length_new - right_length) * mm_to_steps_pulley;
+    
+    left_length = left_length_new;
+    right_length = right_length_new;
     
     stepperL.move(left_steps);
     stepperR.move(right_steps);
@@ -321,8 +326,7 @@ void setPosTest() {
 //make sure to setCurrentPolygon in setup
 void drawPolyTest() {
   if (drawPolygon()) {
-    p.points_arr_index = 0;
-    setCurrentPolygon(p);
+    poly_current.points_arr_index = 0;
   }
   drawPolygon();
 }
