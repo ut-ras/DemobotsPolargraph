2fac0d969081037f2e6a6c23f31bbcac123950a9
benbelov <benbelov@gmail.com>
2018-04-12 22:51:50 -0500

    Merge and debug for JSON array

--- pathing/wifisend.py
+++ pathing/wifisend.py
@@ -4,12 +4,11 @@ import sys
 import serial
 from xml.dom import minidom
 from svg.path import parse_path
+#from IPython import embed
+#from matplotlib.pyplot import *
 import requests
 
-<<<<<<< HEAD
 POLARGRAPH_IP = "http://172.20.10.2/";
-=======
->>>>>>> 4d80edac75cf9d4ddd30f2812395b80872de00fc
 
 filename = sys.argv[1]
 svg_file = open(filename)
@@ -21,18 +20,8 @@ outputfile=open(output, 'w')
 svg_dom = minidom.parseString(svg_string)
 path_strings = [path.getAttribute('d') for path in svg_dom.getElementsByTagName('path')]
 
-<<<<<<< HEAD
-try:
-    ser =  serial.Serial(port, 9600)
-    print('Serial connected to port {}.'.format(port))
-except:
-    print('Serial error! Exiting.')
-    sys.exit(0)
+code = '{ "array": [ '
 
-code = '{ "array": ['
-
-=======
->>>>>>> 4d80edac75cf9d4ddd30f2812395b80872de00fc
 for path_string in path_strings:
     path_data = parse_path(path_string)
 
@@ -61,11 +50,10 @@ for path_string in path_strings:
     for index, item in enumerate(x):
         x[index] = round((x[index]+shift_x)*scale_x)
         y[index] = round((y[index]+shift_y)*scale_y)
-<<<<<<< HEAD
         code += '"({},{})", '.format(x[index], y[index])
         #outputfile.write(code)
         #print("Writing '{}'.".format(code))
-    code = code[:-1] + "] }"
+    code = code[:-2] + " ] }"
 
     try:
         #data = str.encode(code)
@@ -73,25 +61,10 @@ for path_string in path_strings:
         s = requests.post(url = POLARGRAPH_IP, data = code)
         print("Received '{}' in response; continuing.".format(s))
     except:
-        print("Serial error!")
-=======
-        code = '({},{})'.format(x[index], y[index])
-        outputfile.write(code)
-        print("Writing '{}'.".format(code))
-        #try:
-        data = str.encode(code)
-        r = requests.post(url = POLARGRAPH_IP, data = data)
-        print("Received '{}' in response; continuing.".format(r))
-        #except:
-        #    print("HTTP error!")
->>>>>>> 4d80edac75cf9d4ddd30f2812395b80872de00fc
+        print("HTTP error!")
+
 
     print('Complete.')
 
-    ser.write(str.encode('(0,0)'))
-    s = ser.read()
 
-    plot(x,y);show()
-    #embed()
 outputfile.close()
-ser.close()
